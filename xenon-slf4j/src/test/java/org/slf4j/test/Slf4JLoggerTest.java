70565e29abcf55ce06d041647c0ba8e4e8679bd1
Dmitry Korotkov <dkorotkov@vmware.com>
2016-01-11 17:16:09 -0800

    Bump up SLF4J version for logging bridge.
    Hardening SLF4J bridge unit test.
    
    Change-Id: Id7be2f0453ea53ded1053ae02e964601e78b810b

--- xenon-slf4j/src/test/java/org/slf4j/test/Slf4JLoggerTest.java
+++ xenon-slf4j/src/test/java/org/slf4j/test/Slf4JLoggerTest.java
@@ -14,6 +14,7 @@
 package org.slf4j.test;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -25,6 +26,7 @@ import org.slf4j.LoggerFactory;
 
 import com.vmware.xenon.common.BasicTestCase;
 import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.Operation.CompletionHandler;
 import com.vmware.xenon.common.UriUtils;
 import com.vmware.xenon.services.common.ServiceHostLogService.LogServiceState;
 import com.vmware.xenon.services.common.ServiceUriPaths;
@@ -34,7 +36,7 @@ import com.vmware.xenon.services.common.ServiceUriPaths;
  */
 public class Slf4JLoggerTest extends BasicTestCase {
     private static final Logger logger = LoggerFactory.getLogger(Slf4JLoggerTest.class);
-    public static final long SHORT_TIMEOUT_SEC = 1;
+    public static final int SHORT_TIMEOUT_SEC = 1;
 
     @Test
     public void testLogger() throws Throwable {
@@ -90,20 +92,37 @@ public class Slf4JLoggerTest extends BasicTestCase {
         logger.warn("This message should not present");
         logger.error("This message should not present");
 
+        host.setLoggingLevel(Level.SEVERE);
+        LogManager.getLogManager().getLogger("").setLevel(Level.SEVERE);
+
+        final String endOfLogMarker = UUID.randomUUID().toString();
+        logger.error(endOfLogMarker);
+
         host.testStart(1);
         AtomicReference<LogServiceState> stateRef = new AtomicReference<>();
         Operation.createGet(UriUtils.buildUri(host, ServiceUriPaths.PROCESS_LOG))
                 .setReferer(host.getPublicUri())
-                .setCompletion((op, ex) -> {
-                    if (ex == null) {
-                        stateRef.set(op.getBody(LogServiceState.class));
-                        host.completeIteration();
-                    } else {
-                        host.failIteration(ex);
+                .setCompletion(new CompletionHandler() {
+                    @Override
+                    public void handle(Operation op, Throwable ex) {
+                        if (ex == null) {
+                            LogServiceState log = op.getBody(LogServiceState.class);
+                            if (log.items.stream().filter(s -> s.contains(endOfLogMarker)).count() > 0) {
+                                stateRef.set(log);
+                                host.completeIteration();
+                            } else {
+                                Operation.createGet(UriUtils.buildUri(host, ServiceUriPaths.PROCESS_LOG))
+                                        .setReferer(host.getPublicUri())
+                                        .setCompletion(this)
+                                        .sendWith(host);
+                            }
+                        } else {
+                            host.failIteration(ex);
+                        }
                     }
                 })
                 .sendWith(host);
-        host.testWait(1);
+        host.testWait(SHORT_TIMEOUT_SEC);
         LogServiceState state = stateRef.get();
         checkLogLine(state.items, "This is a trace test with no args");
         checkLogLine(state.items, "This is a trace test with 1 arg: argument");
