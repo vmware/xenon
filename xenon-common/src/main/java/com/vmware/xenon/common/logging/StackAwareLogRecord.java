bb059bc33bb3533812e1fc421a2c9acbec8ee07a
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2016-02-10 15:30:59 -0800

    Stack aware LogRecord
    Add StackAwareLogRecord to hold detailed context data for logging invocation.
    
    Change-Id: I973179b89467f45ff05a6fe4541a2eb95eed5d7f

---
+++ xenon-common/src/main/java/com/vmware/xenon/common/logging/StackAwareLogRecord.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2014-2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.common.logging;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+
+/**
+ * LogRecord that contains invoked line of code StackTraceElement.
+ *
+ * StackTraceElement contains detailed information about where log method was called.
+ */
+public class StackAwareLogRecord extends LogRecord {
+
+    private static final long serialVersionUID = 2233534645443849237L;
+    private StackTraceElement stack;
+
+    public StackAwareLogRecord(Level level, String msg) {
+        super(level, msg);
+    }
+
+    public String getStackClassName() {
+        return this.stack == null ? null : this.stack.getClassName();
+    }
+
+    public String getStackMethodName() {
+        return this.stack == null ? null : this.stack.getMethodName();
+    }
+
+    public String getStackFileName() {
+        return this.stack == null ? null : this.stack.getFileName();
+    }
+
+    public int getStackLineNumber() {
+        return this.stack == null ? 0 : this.stack.getLineNumber();
+    }
+
+    public StackTraceElement getStack() {
+        return this.stack;
+    }
+
+    public void setStack(StackTraceElement stack) {
+        this.stack = stack;
+    }
+}
