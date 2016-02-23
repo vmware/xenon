0f7a5da852491d63cff735eb53dcd5cd3b95d527
Julian Vassev <jvassev@vmware.com>
2016-02-23 16:54:52 +0200

    Add VerificationHost.guard() method
    It wrapps a CompletionHandler in try/catch and completes/fails
    the current iteration saving some boilerplate.
    
    The TestSampleServiceWithCustomUi
    test is changed to use guard() as an example.
    
    Change-Id: I81eb7e9cbe0ea339e965658ff8466a3243823f4b

--- xenon-samples/src/test/java/com/vmware/dcp/services/samples/TestSampleServiceWithCustomUi.java
+++ xenon-samples/src/test/java/com/vmware/dcp/services/samples/TestSampleServiceWithCustomUi.java
@@ -13,6 +13,10 @@
 
 package com.vmware.dcp.services.samples;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
 import java.net.URI;
 
 import org.junit.Before;
@@ -61,17 +65,10 @@ public class TestSampleServiceWithCustomUi extends BasicReusableHostTestCase {
     public void testGetUi() throws Throwable {
         Operation op = Operation
                 .createGet(UriUtils.buildUri(this.host, THE_SERVICE_URI + "/ui"))
-                .setCompletion((o, e) -> {
-                    if (e != null) {
-                        this.host.failIteration(e);
-                        return;
-                    }
-                    if (o.getStatusCode() != Operation.STATUS_CODE_MOVED_TEMP) {
-                        this.host.failIteration(new AssertionError("did not redirect to ui/"));
-                    } else {
-                        this.host.completeIteration();
-                    }
-                });
+                .setCompletion(guard((o, e) -> {
+                    assertNull(e);
+                    assertEquals(Operation.STATUS_CODE_MOVED_TEMP, o.getStatusCode());
+                }));
 
         this.host.testStart(1);
         this.host.send(op);
@@ -82,21 +79,11 @@ public class TestSampleServiceWithCustomUi extends BasicReusableHostTestCase {
     public void testGetUiWithSlash() throws Throwable {
         Operation op = Operation
                 .createGet(URI.create(UriUtils.buildUri(this.host, THE_SERVICE_URI + "/ui") + "/"))
-                .setCompletion((o, e) -> {
-                    if (e != null) {
-                        this.host.failIteration(e);
-                        return;
-                    }
-                    if (o.getStatusCode() != Operation.STATUS_CODE_OK) {
-                        this.host.failIteration(new AssertionError("Expected 200 OK"));
-                    } else {
-                        if (o.getBody(String.class).contains("DOCTYPE")) {
-                            this.host.completeIteration();
-                        } else {
-                            this.host.failIteration(new AssertionError("Did not GET the /ui/index.html"));
-                        }
-                    }
-                });
+                .setCompletion(guard((o, e) -> {
+                    assertNull(e);
+                    assertEquals(Operation.STATUS_CODE_OK, o.getStatusCode());
+                    assertTrue(o.getBody(String.class).contains("DOCTYPE"));
+                }));
 
         this.host.testStart(1);
         this.host.send(op);
@@ -107,21 +94,11 @@ public class TestSampleServiceWithCustomUi extends BasicReusableHostTestCase {
     public void testGetResource() throws Throwable {
         Operation op = Operation
                 .createGet(URI.create(UriUtils.buildUri(this.host, THE_SERVICE_URI + "/ui/README.txt") + "/"))
-                .setCompletion((o, e) -> {
-                    if (e != null) {
-                        this.host.failIteration(e);
-                        return;
-                    }
-                    if (o.getStatusCode() != Operation.STATUS_CODE_OK) {
-                        this.host.failIteration(new AssertionError("Expected 200 OK"));
-                    } else {
-                        if (o.getBody(String.class).contains("unit-test")) {
-                            this.host.completeIteration();
-                        } else {
-                            this.host.failIteration(new AssertionError("Did not GET the README.txt"));
-                        }
-                    }
-                });
+                .setCompletion(guard((o, e) -> {
+                    assertNull(e);
+                    assertEquals(Operation.STATUS_CODE_OK, o.getStatusCode());
+                    assertTrue(o.getBody(String.class).contains("unit-test"));
+                }));
 
         this.host.testStart(1);
         this.host.send(op);
