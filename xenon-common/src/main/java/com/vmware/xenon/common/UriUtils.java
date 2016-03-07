1a11e2f46897a540f7c5409b58a3c6862f5317d4
Doron Gold <dgoldgevicht@vmware.com>
2016-03-07 18:48:29 +0200

    Fix exception thrown by buildUri
    When a string ending with the '?' character is
    given to UriUtils.buildUri() or to UriUtils.extendUri()
    an ArrayIndexOutOfBoundsException is thrown.
    This fix prevents that exception.
    
    Instead, if the parameter string ends with a
    single '?' character, it will be ignored.
    
    Change-Id: Ic70be1c935fd8b4de2cb0cdc544e7e745b739817

--- xenon-common/src/main/java/com/vmware/xenon/common/UriUtils.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/UriUtils.java
@@ -164,11 +164,12 @@ public class UriUtils {
     public static URI extendUri(URI uri, String path) {
         String query = null;
         if (path != null && path.contains(UriUtils.URI_QUERY_CHAR)) {
-            String[] pathAndQuery = path.split("\\" + UriUtils.URI_QUERY_CHAR);
-            path = pathAndQuery[0];
-            query = pathAndQuery[1];
+            final int indexOfFirstQueryChar = path.indexOf(UriUtils.URI_QUERY_CHAR);
+            if (indexOfFirstQueryChar < path.length() - 1) {
+                query = path.substring(indexOfFirstQueryChar + 1, path.length());
+            }
+            path = path.substring(0, indexOfFirstQueryChar);
         }
-
         return buildUri(uri.getScheme(), uri.getHost(), uri.getPort(),
                 normalizeUriPath(uri.getPath()) + normalizeUriPath(path),
                 query);
@@ -190,9 +191,11 @@ public class UriUtils {
     public static URI buildUri(String scheme, String host, int port, String path, String query) {
         try {
             if (path != null && path.contains(UriUtils.URI_QUERY_CHAR)) {
-                String[] pathAndQuery = path.split("\\" + UriUtils.URI_QUERY_CHAR);
-                path = pathAndQuery[0];
-                query = pathAndQuery[1];
+                final int indexOfFirstQueryChar = path.indexOf(UriUtils.URI_QUERY_CHAR);
+                if (indexOfFirstQueryChar < path.length() - 1) {
+                    query = path.substring(indexOfFirstQueryChar + 1, path.length());
+                }
+                path = path.substring(0, indexOfFirstQueryChar);
             }
             path = normalizeUriPath(path);
             return new URI(scheme, null, host, port, path, query, null).normalize();
@@ -280,13 +283,16 @@ public class UriUtils {
                 continue;
             }
             if (p.contains(UriUtils.URI_QUERY_CHAR)) {
-                String[] pathAndQuery = p.split("\\" + UriUtils.URI_QUERY_CHAR);
-                p = pathAndQuery[0];
-                if (query == null) {
-                    query = pathAndQuery[1];
-                } else {
-                    query += pathAndQuery[1];
+                final int indexOfFirstQueryChar = p.indexOf(UriUtils.URI_QUERY_CHAR);
+                if (indexOfFirstQueryChar < p.length() - 1) {
+                    final String curQuery = p.substring(indexOfFirstQueryChar + 1, p.length());
+                    if (query == null) {
+                        query = curQuery;
+                    } else {
+                        query += curQuery;
+                    }
                 }
+                p = p.substring(0, indexOfFirstQueryChar);
             }
             p = normalizeUriPath(p);
             if (buildPath == null) {
