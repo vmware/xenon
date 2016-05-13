7559a2ad0f86c6ea0c4c01f0f71c75b7fbd8cd0d
Julian Vassev <jvassev@vmware.com>
2016-05-14 00:37:21 +0300

    Enable Operation composition using CompletableFuture
    Allow operations sequencing, blocking on completion, fork/join to be done with
    CompletableFuture. TestFutures class gives examples how to use it.
    
    Promises/Futures are only used in test code for now to prevent users from
    accidentally using blocking code in their service implementations.
    
    Borrows some ideas from
    http://blog.krecan.net/2013/12/25/completablefutures-why-to-use-async-methods/
    Change-Id: I4414ecc524cdabf309ebfeb6f91cd5d130389b04

--- xenon-common/src/test/java/com/vmware/xenon/common/test/VerificationHost.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/test/VerificationHost.java
@@ -41,6 +41,7 @@ import java.util.Map.Entry;
 import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -567,6 +568,25 @@ public class VerificationHost extends ExampleServiceHost {
         minState.stringValue = sb.toString();
 
         return minState;
+    }
+
+    public CompletableFuture<Operation> sendWithFuture(Operation op) {
+        if (op.getCompletion() != null) {
+            throw new IllegalStateException("completion handler must not be set");
+        }
+
+        CompletableFuture<Operation> res = new CompletableFuture<>();
+        op.setCompletion((o, e) -> {
+            if (e != null) {
+                res.completeExceptionally(e);
+            } else {
+                res.complete(o);
+            }
+        });
+
+        this.send(op);
+
+        return res;
     }
 
     /**
