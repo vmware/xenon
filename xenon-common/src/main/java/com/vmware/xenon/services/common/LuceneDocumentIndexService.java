c784045d3f285873d2c5ca87ae82c254b0d453c4
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2016-06-10 16:01:52 -0700

    Remove empty last page in pagination
    Prior to this commit, paginated query always had last page
    that contains empty result.
    In this commit, changed nextPageLink generation logic NOT to
    create thd link to the last empty result page.
    Also updated MigrationTaskService to accomodate the change
    
    https://www.pivotaltracker.com/story/show/119261665
    
    Change-Id: I27741d51d08869bd8a630be865587ac4a3f81da5

--- xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
@@ -964,11 +964,23 @@ public class LuceneDocumentIndexService extends StatelessService {
                 if (!hasPage || rsp.documentLinks.size() >= count
                         || hits.length < resultLimit) {
                     // query had less results then per page limit or page is full of results
-                    expiration += queryTime;
-                    rsp.nextPageLink = createNextPage(op, s, options, tq, sort, bottom, count,
-                            expiration,
-                            indexLink,
-                            hasPage);
+
+                    boolean createNextPageLink;
+                    if (!hasPage) {
+                        createNextPageLink = true;
+                    } else {
+                        createNextPageLink = checkNextPageHasEntry(bottom, options, s, tq, sort,
+                                count, qs, queryStartTimeMicros);
+                    }
+
+                    if (createNextPageLink) {
+                        expiration += queryTime;
+                        rsp.nextPageLink = createNextPage(op, s, options, tq, sort, bottom, count,
+                                expiration,
+                                indexLink,
+                                hasPage);
+                    }
+
                     break;
                 }
             }
@@ -978,6 +990,50 @@ public class LuceneDocumentIndexService extends StatelessService {
         } while (true && resultLimit > 0);
 
         return rsp;
+    }
+
+    private boolean checkNextPageHasEntry(ScoreDoc after,
+            EnumSet<QueryOption> options,
+            IndexSearcher s,
+            Query tq,
+            Sort sort,
+            int count,
+            QuerySpecification qs,
+            long queryStartTimeMicros) throws Throwable {
+
+        boolean hasValidNextPageEntry = false;
+
+        while (after != null) {
+
+            // fetch next page
+            TopDocs nextPageResults;
+            if (sort == null) {
+                nextPageResults = s.searchAfter(after, tq, count);
+            } else {
+                nextPageResults = s.searchAfter(after, tq, count, sort, false, false);
+            }
+            if (nextPageResults == null) {
+                break;
+            }
+
+            ScoreDoc[] hits = nextPageResults.scoreDocs;
+            if (hits.length == 0) {
+                // reached to the end
+                break;
+            }
+
+            ServiceDocumentQueryResult rspForNextPage = new ServiceDocumentQueryResult();
+            rspForNextPage.documents = new HashMap<>();
+            after = processQueryResults(qs, options, count, s, rspForNextPage, hits,
+                    queryStartTimeMicros);
+
+            if (rspForNextPage.documentCount > 0) {
+                hasValidNextPageEntry = true;
+                break;
+            }
+        }
+
+        return hasValidNextPageEntry;
     }
 
     private String createNextPage(Operation op, IndexSearcher s, EnumSet<QueryOption> options,
