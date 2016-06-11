6fd64d34d122ae78bf9e463d486738d78587ba67
sufiand <dars@vmware.com>
2016-06-10 18:19:01 -0700

    WIP: Investigating version retention test failure
    Change-Id: I76f00d17816114d59af2a7de2765df68cade5ae4

--- xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/LuceneDocumentIndexService.java
@@ -1711,7 +1711,7 @@ public class LuceneDocumentIndexService extends StatelessService {
     private void deleteAllDocumentsForSelfLink(Operation postOrDelete, String link,
             ServiceDocument state)
                     throws Throwable {
-        deleteDocumentsFromIndex(postOrDelete, link, 0);
+        deleteDocumentsFromIndex(postOrDelete, link, 0, false);
         ServiceStat st = getStat(STAT_NAME_SERVICE_DELETE_COUNT);
         adjustStat(st, 1);
         logFine("%s expired", link);
@@ -1733,7 +1733,7 @@ public class LuceneDocumentIndexService extends StatelessService {
      * @throws Throwable
      */
     private void deleteDocumentsFromIndex(Operation delete, String link,
-            long versionsToKeep) throws Throwable {
+            long versionsToKeep, boolean versionRetention) throws Throwable {
         IndexWriter wr = this.writer;
         if (wr == null) {
             delete.fail(new CancellationException());
@@ -1795,8 +1795,10 @@ public class LuceneDocumentIndexService extends StatelessService {
 
         results = s.search(bq, Integer.MAX_VALUE);
 
-        logInfo("trimming index for %s from %d to %d, query returned %d", link, hits.length,
-                versionsToKeep, results.totalHits);
+        logInfo("*** Trimming index for %s from %d to %d as part of %s, Total count: %d",
+                link, versionLowerBound, versionUpperBound,
+                versionRetention ? "version retention" : "document deletion",
+                results.totalHits);
 
         wr.deleteDocuments(bq);
         long now = Utils.getNowMicrosUtc();
@@ -2102,11 +2104,12 @@ public class LuceneDocumentIndexService extends StatelessService {
 
             int pastRetentionLimitVersions = (int) (documentCount - e.getValue());
             if (pastRetentionLimitVersions <= 0) {
+                logInfo("*** Skipping retention for %s. Found documentCount %d", e.getKey(), documentCount);
                 continue;
             }
 
             // trim durable index for this link
-            deleteDocumentsFromIndex(dummyDelete, e.getKey(), e.getValue());
+            deleteDocumentsFromIndex(dummyDelete, e.getKey(), e.getValue(), true);
             count++;
         }
 
