dac234c9f6b77e146da0c5e9adfbab00fb554b41
George Chrysanthakopoulos <georgec@vmware.com>
2016-06-14 12:26:13 -0700

    Fix QueryOption.EXPAND_LINKS test failures
    I think the CI test failures are because we manipulate a map while
    traversing it.
    
    Also fix additional failure mode: we should schedule expiration only
    when expand links completes.
    
    https://www.pivotaltracker.com/story/show/121513963
    https://www.pivotaltracker.com/story/show/121518801
    
    Change-Id: I4a309caedb76ab7f8d0e5372a6014c6d4d67a43d

--- xenon-common/src/main/java/com/vmware/xenon/services/common/QueryTaskService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/QueryTaskService.java
@@ -540,7 +540,13 @@ public class QueryTaskService extends StatefulService {
                 task.documentOwner = getHost().getId();
             }
 
+            scheduleExpiration = !task.querySpec.options.contains(QueryOption.EXPAND_LINKS);
             if (directOp != null) {
+                directOp.nestCompletion((o, ex) -> {
+                    directOp.setStatusCode(o.getStatusCode())
+                            .setBodyNoCloning(o.getBodyRaw()).complete();
+                    scheduleTaskExpiration(task);
+                });
                 QueryTaskUtils.expandLinks(getHost(), task, directOp);
             } else {
                 if (!task.querySpec.options.contains(QueryOption.EXPAND_LINKS)) {
@@ -549,6 +555,7 @@ public class QueryTaskService extends StatefulService {
                 }
 
                 CompletionHandler c = (o, ex) -> {
+                    scheduleTaskExpiration(task);
                     if (ex != null) {
                         failTask(ex, null, null);
                         return;
