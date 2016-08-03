11602001da63669071b5c111c2ecb2fa7fac7fcf
JJ <janislav@vmware.com>
2016-08-02 18:10:17 -0700

    Add new send request pattern with DeferredResult
    The new sendWithDeferredResult set of methods return DeferredResult
    instances to enable monadic style of code.
    This has several advantages over the original callbacks style code,
    to name a few: avoid callback hell (nesting callbacks); more elegant
    way to conclude an operation -- using whenComplete* which is
    guaranteed to be invoked even if there is a failure in the prior
    stages; simpler error handling and recovery.
    
    Check the refactored SamplePreviousEchoService and
    SampleBootstrapService for usage examples.
    
    Update 8/15:
    - Unit tests
    
    Update 8/2:
    - Narrowed down the scope to only enable the async/promise/deferred -style
    - Renamed Promise -> DeferredResult
    
    Update 7/21:
    - Implementation of Promise based pretty much entirely on CompletableFuture but without the
       blocking and the implicit executor methods.
    - Separated the responsibilities of the Operation into Response/Request
    - Replaced TypedStatefulService with SimpleStateAwareRequestsHandler
    - Added the more generic StandardStateAwareRequestsHandler
    
    Initial:
    - Added a new generic class TypedStatefulService to enforce static types in the handlers
    - "async" sendRequest method returning CompletableFuture (CF) is added to the ServiceRequestSender.
       I didn't name it asyncRequestSender as the "async" keyword is used with the CF's and didn't
       want to cause confusion. Naming suggestions appreciated
    - The modified samples are for illustration purposes to highlight the differences
    
    Change-Id: I103dc93c5fbdab7d0750d59412fca9666200a518

--- xenon-common/src/test/java/com/vmware/xenon/common/test/VerificationHost.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/test/VerificationHost.java
@@ -44,11 +44,13 @@ import java.util.Random;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -65,6 +67,7 @@ import org.junit.rules.TemporaryFolder;
 
 import com.vmware.xenon.common.Claims;
 import com.vmware.xenon.common.CommandLineArgumentParser;
+import com.vmware.xenon.common.DeferredResult;
 import com.vmware.xenon.common.NodeSelectorState;
 import com.vmware.xenon.common.Operation;
 import com.vmware.xenon.common.Operation.AuthorizationContext;
@@ -257,6 +260,19 @@ public class VerificationHost extends ExampleServiceHost {
         };
     }
 
+    public <T> BiConsumer<T, ? super Throwable> getCompletionDeferred() {
+        return (ignore, e) -> {
+            if (e != null) {
+                if (e instanceof CompletionException) {
+                    e = e.getCause();
+                }
+                failIteration(e);
+                return;
+            }
+            completeIteration();
+        };
+    }
+
     public CompletionHandler getExpectedFailureCompletion() {
         return getExpectedFailureCompletion(null);
     }
@@ -315,6 +331,18 @@ public class VerificationHost extends ExampleServiceHost {
         super.sendRequest(op);
     }
 
+    @Override
+    public DeferredResult<Operation> sendWithDeferredResult(Operation operation) {
+        operation.setReferer(getReferer());
+        return super.sendWithDeferredResult(operation);
+    }
+
+    @Override
+    public <T> DeferredResult<T> sendWithDeferredResult(Operation operation, Class<T> resultType) {
+        operation.setReferer(getReferer());
+        return super.sendWithDeferredResult(operation, resultType);
+    }
+
     /**
      * Creates a test wait context that can be nested and isolated from other wait contexts
      */
@@ -331,6 +359,7 @@ public class VerificationHost extends ExampleServiceHost {
 
     /**
      * Starts a test context used for a single synchronous test execution for the entire host
+     * @param c Expected completions
      */
     public void testStart(long c) {
         if (this.isSingleton) {
@@ -448,8 +477,6 @@ public class VerificationHost extends ExampleServiceHost {
             this.context = null;
             this.lastTestName = testName;
         }
-        return;
-
     }
 
     public double calculateThroughput() {
