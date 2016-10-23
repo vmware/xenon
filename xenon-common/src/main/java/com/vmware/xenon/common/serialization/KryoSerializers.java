4401bf3b8f39908d9e91bf3d18c2aacbaa5905a2
George Chrysanthakopoulos <georgec@vmware.com>
2016-10-23 09:56:47 -0700

    Fix Utils.encodeBody, refactor binary serialization utils
    - Utils.encodeBody went through a potentially expensive re-allocation
      loop, depending on body size. Using the NIO Output class, it takes
      care of allocating buffers as necessery and doing streaming serialization
    - use Kryo NIO Output class to return result of serialization
    - remove methods from Utils that are in KryoSerializers
    - simplify buffer management
    
    Change-Id: Iea71d4dbf9c33830680b1cdec8dc50280b272da0

--- xenon-common/src/main/java/com/vmware/xenon/common/serialization/KryoSerializers.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/serialization/KryoSerializers.java
@@ -24,8 +24,8 @@ import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.Kryo.DefaultInstantiatorStrategy;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
-
 import com.esotericsoftware.kryo.serializers.VersionFieldSerializer;
+
 import org.objenesis.strategy.StdInstantiatorStrategy;
 
 import com.vmware.xenon.common.ServiceDocument;
@@ -123,31 +123,30 @@ public final class KryoSerializers {
      * The document should not contain circular references.
      * Must be paired with {@code KryoSerializers#deserializeDocument(byte[], int, int)}
      */
-    public static int serializeDocument(ServiceDocument o, byte[] buffer, int position) {
-        return serializeAsDocument(o, buffer, position);
+    public static Output serializeDocument(ServiceDocument o, int maxSize) {
+        return serializeAsDocument(o, maxSize);
     }
 
     /**
      * See {@link #serializeDocument(ServiceDocument, byte[], int)}
      */
-    public static int serializeAsDocument(Object o, byte[] buffer, int position) {
+    public static Output serializeAsDocument(Object o, int maxSize) {
         Kryo k = getKryoThreadLocalForDocuments();
-        Output out = new Output(buffer);
-        out.setPosition(position);
+        Output out = new Output(getBuffer(4096), maxSize);
         k.writeClassAndObject(out, o);
-        return out.position();
+        return out;
     }
 
     /**
-     * Uses custom serialization that will write nulls for select built-in fields that can
-     * be reconstructed from other index data
+     * Infrastructure use only. Uses custom serialization that will write nulls for select built-in
+     * fields that can be reconstructed from other index data
      */
-    public static int serializeObjectForIndexing(Object o, byte[] buffer, int position) {
+    public static Output serializeDocumentForIndexing(Object o, int maxSize) {
         Kryo k = getKryoThreadLocalForDocuments();
-        Output out = new OutputWithRoot(buffer, o);
-        out.setPosition(position);
+        byte[] buffer = getBuffer(4096);
+        Output out = new OutputWithRoot(buffer, maxSize, o);
         k.writeClassAndObject(out, o);
-        return out.position();
+        return out;
     }
     /**
      * Serializes an arbitrary object into a binary representation, using full
