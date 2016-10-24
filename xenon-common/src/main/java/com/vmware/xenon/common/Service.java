0ddf9a3da7b74d65e5d200114d2949b47d2d2df0
Vamsi Krishna Brahmajosyula <vbrahmajosyula@vmware.com>
2016-10-24 20:44:51 +0530

    Validate service options for Stateless Services.
    1) Add option ServiceOption.STATELESS
    2) Enabled on StatelessService, Disabled on FactoryService.
    3) STATLESS conflicts with PERSISTENCE, REPLICATION,
       OWNER_SELECTION, STRICT_UPDATE_CHECKING.
    
    Change-Id: I1a5f61cee589b874b56cd1c5142fc8d098e08fa0

--- xenon-common/src/main/java/com/vmware/xenon/common/Service.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/Service.java
@@ -219,6 +219,10 @@ public interface Service extends ServiceRequestSender {
          */
         TRANSACTION_PENDING,
 
+        /**
+         * Set by runtime. Service is stateless.
+         */
+        STATELESS,
         NONE
     }
 
