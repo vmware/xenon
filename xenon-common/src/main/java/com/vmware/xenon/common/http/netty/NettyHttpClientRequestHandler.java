a1bf94791cad43781236e7d071c09c786189782c
George Chrysanthakopoulos <georgec@vmware.com>
2016-11-01 17:11:57 -0700

    WIP Request rate throttling improvements
    not ready for review
    
    Enable options on request rate limiting and apply it only
    on remote requests
    
    Part of this change is properly identifying remote requests since existing
    listener request handler was not properly marking inbound operations.
    
    Fix rate limit tests: it turns out the tests were passing because we were getting
    authz related failures, not throttling induced failures.
    
    First step relating to this feature:
    https://www.pivotaltracker.com/story/show/133482593
    
    Change-Id: Id5edbedf3775f4f777282c1b39cbdcf9a7810774

--- xenon-common/src/main/java/com/vmware/xenon/common/http/netty/NettyHttpClientRequestHandler.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/http/netty/NettyHttpClientRequestHandler.java
@@ -94,7 +94,8 @@ public class NettyHttpClientRequestHandler extends SimpleChannelInboundHandler<O
             URI targetUri = new URI(nettyRequest.uri()).normalize();
             request = Operation.createGet(null);
             request.setAction(Action.valueOf(nettyRequest.method().toString()))
-                    .setExpiration(expMicros);
+                    .setExpiration(expMicros)
+                    .forceRemote();
 
             String query = targetUri.getQuery();
             if (query != null && !query.isEmpty()) {
