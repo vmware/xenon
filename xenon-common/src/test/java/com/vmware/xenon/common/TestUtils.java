87682ba8d0828e2cdaaed8d3b2de44bc5fed91a1
George Chrysanthakopoulos <georgec@vmware.com>
2016-11-09 20:43:08 -0800

    replace murmur3 with FNV1 variant
    Recent profilng identified murmur3 as a hot spot. Further analysis
    with checks for collisions revealed a large number of collisions for
    closely related strings (which is common for self links).
    For 8M keys, we observed 7000+ collisions.
    
    FNV algorithm is 10% using the new TestUtils.computeHash micro benchmark and as measured
    through directOwnerSelection test.
    Its not cryptographically safe, which is ok for the routing, signature
    calculations
    
    The new has also has some other surprising benefits:
    - With 8 millions keys, of similar, but unique strings, murmur3 produces nearly 11K
      collisions, but FNV produces zero collisions
    - The consistent hashing of 30K random keys among 3 nodes, produces a very well balanced
      ring with nodes being within 100 keys of each other (nearly perfect 10K each). Murmur3
      always produced large differences with small number of nodes (some nodes would own 15K
      keys while others would own 6k, 8k, etc)
    
    Change-Id: I6f1a2f019290b090992431abf24e2b65fb357333

--- xenon-common/src/test/java/com/vmware/xenon/common/TestUtils.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/TestUtils.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.net.URLEncoder;
@@ -32,6 +33,7 @@ import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -125,6 +127,66 @@ public class TestUtils {
         } finally {
             Utils.setTimeDriftThreshold(Utils.DEFAULT_TIME_DRIFT_THRESHOLD_MICROS);
         }
+    }
+
+    @Test
+    public void computeStringHash() {
+        CommandLineArgumentParser.parseFromProperties(this);
+        Set<String> keys = new HashSet<>(this.iterationCount);
+        Set<Long> hashedKeys = new HashSet<>(this.iterationCount);
+        long collisionCount = 0;
+
+        for (int i = 0; i < this.iterationCount; i++) {
+            String k = "-string-" + i;
+            long hash = FNVHash.compute(k);
+            long hash2 = FNVHash.compute(k);
+            assertEquals(hash, hash2);
+            assertTrue(keys.add(k));
+        }
+        Logger.getAnonymousLogger().info("Generated keys: " + keys.size());
+        long s = System.nanoTime() / 1000;
+        for (String key : keys) {
+            long hash = FNVHash.compute(key);
+            assertTrue(hash != 0);
+            if (!hashedKeys.add(hash)) {
+                collisionCount++;
+            }
+        }
+        long e = System.nanoTime() / 1000;
+        double thpt = this.iterationCount / ((e - s) / 1000000.0);
+        Logger.getAnonymousLogger().info("Throughput: " + thpt);
+        Logger.getAnonymousLogger().info("Collisions: " + collisionCount);
+    }
+
+    @Test
+    public void computeByteHash() throws UnsupportedEncodingException {
+        CommandLineArgumentParser.parseFromProperties(this);
+        Set<String> keys = new HashSet<>(this.iterationCount);
+        Set<Long> hashedKeys = new HashSet<>(this.iterationCount);
+        long collisionCount = 0;
+
+        for (int i = 0; i < this.iterationCount; i++) {
+            String k = "-string-" + i;
+            byte[] bytes = k.getBytes(Utils.CHARSET);
+            long hash = FNVHash.compute(bytes, 0, bytes.length);
+            long hash2 = FNVHash.compute(bytes, 0, bytes.length);
+            assertEquals(hash, hash2);
+            assertTrue(keys.add(k));
+        }
+        Logger.getAnonymousLogger().info("Generated keys: " + keys.size());
+        long s = System.nanoTime() / 1000;
+        for (String key : keys) {
+            byte[] bytes = key.getBytes(Utils.CHARSET);
+            long hash = FNVHash.compute(bytes, 0, bytes.length);
+            assertTrue(hash != 0);
+            if (!hashedKeys.add(hash)) {
+                collisionCount++;
+            }
+        }
+        long e = System.nanoTime() / 1000;
+        double thpt = this.iterationCount / ((e - s) / 1000000.0);
+        Logger.getAnonymousLogger().info("Throughput: " + thpt);
+        Logger.getAnonymousLogger().info("Collisions: " + collisionCount);
     }
 
     @Test
@@ -286,7 +348,7 @@ public class TestUtils {
             // serialize with defaults, then with custom, and compare sizes
 
             int byteCountToObjectDefault = Utils.toBytes(st, Utils.getBuffer(1024), 0);
-            Output outDocumentImplicitDefault = KryoSerializers.serializeAsDocument((Object) st,
+            Output outDocumentImplicitDefault = KryoSerializers.serializeAsDocument(st,
                     1024);
             Output outDocumentDefault = KryoSerializers.serializeDocument(st, 1024);
 
@@ -295,8 +357,8 @@ public class TestUtils {
                     true);
 
             byte[] objectData = new byte[1024];
-            int byteCountToObjectCustom = Utils.toBytes((Object) st, objectData, 0);
-            Output outDocumentImplicitCustom = KryoSerializers.serializeAsDocument((Object) st,
+            int byteCountToObjectCustom = Utils.toBytes(st, objectData, 0);
+            Output outDocumentImplicitCustom = KryoSerializers.serializeAsDocument(st,
                     1024);
             Output outDocumentCustom = KryoSerializers.serializeDocument(st, 1024);
 
