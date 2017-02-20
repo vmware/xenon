bb7801d9f0b1a243c19dd3eb038abf93d9a7fbcf
Touseef Liaqat <tliaqat@vmware.com>
2017-02-20 01:57:08 -0800

    Load balanced continuous query sample
    A StatefulService that is responsible for creating the CQ and a Subscription.
    The CQ Service will be created on the local index.
    The StatefulService overrides handleNodeGroupMaintenance to
    stop the CQ & subscription when the node is no longer the owner.
    
    Change-Id: I67d7448cd6c4d866cef06e1e1e442b6c7142e2ae

---
+++ xenon-samples/src/test/java/com/vmware/dcp/services/samples/TestSampleContinuousQueryWatchService.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright (c) 2014-2016 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.dcp.services.samples;
+
+import static java.util.stream.Collectors.toList;
+
+import static com.vmware.xenon.common.test.TestContext.waitFor;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vmware.xenon.common.CommandLineArgumentParser;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.Service.ServiceOption;
+import com.vmware.xenon.common.ServiceStats;
+import com.vmware.xenon.common.ServiceStats.ServiceStat;
+import com.vmware.xenon.common.test.TestRequestSender;
+import com.vmware.xenon.common.test.VerificationHost;
+import com.vmware.xenon.services.common.LuceneDocumentIndexService;
+import com.vmware.xenon.services.samples.SampleContinuousQueryWatchService;
+import com.vmware.xenon.services.samples.SamplePreviousEchoService;
+import com.vmware.xenon.services.samples.SamplePreviousEchoService.EchoServiceState;
+
+
+public class TestSampleContinuousQueryWatchService {
+
+    private VerificationHost host;
+    private TestRequestSender sender;
+    public int serviceCount = 10;
+    private Duration timeout = Duration.ofSeconds(5);
+
+    @Before
+    public void setupHost() throws Throwable {
+        CommandLineArgumentParser.parseFromProperties(this);
+        this.host = VerificationHost.create(0);
+        this.host.start();
+        this.host.toggleServiceOptions(this.host.getDocumentIndexServiceUri(),
+                EnumSet.of(ServiceOption.INSTRUMENTATION), null);
+        this.sender = new TestRequestSender(this.host);
+        this.host.startFactory(new SamplePreviousEchoService());
+        this.host.startFactory(new SampleContinuousQueryWatchService());
+        this.host.waitForServiceAvailable(SamplePreviousEchoService.FACTORY_LINK);
+        this.host.waitForServiceAvailable(SampleContinuousQueryWatchService.FACTORY_LINK);
+    }
+
+    @After
+    public void cleanup() {
+        try {
+            this.host.logServiceStats(this.host.getDocumentIndexServiceUri());
+        } catch (Throwable e) {
+            this.host.log("Error logging stats: %s", e.toString());
+        }
+        this.host.tearDownInProcessPeers();
+        this.host.tearDown();
+    }
+
+    @Test
+    public void testSampleWatchService() throws Throwable {
+        // create a new continuous query watch service with
+        // notification counter set to 0
+        SampleContinuousQueryWatchService.State sampleQueryWatchState = new SampleContinuousQueryWatchService.State();
+        sampleQueryWatchState.notificationsCounter = 0;
+        Operation post = Operation.createPost(this.host, SampleContinuousQueryWatchService.FACTORY_LINK)
+                .setBody(sampleQueryWatchState);
+
+        sampleQueryWatchState = this.sender.sendAndWait(post, SampleContinuousQueryWatchService.State.class);
+        // remember the link to the continuous query watch service we just created
+        final String queryWatchSelfLink = sampleQueryWatchState.documentSelfLink;
+
+        // wait for filter to be active in the index service, which happens asynchronously
+        // in relation to query task creation, before issuing updates.
+        waitFor(this.timeout, () -> {
+            Map<String, ServiceStat> indexStats = this.host.getServiceStats(this.host.getDocumentIndexServiceUri());
+
+            ServiceStat activeQueryStat = indexStats.get(
+                    LuceneDocumentIndexService.STAT_NAME_ACTIVE_QUERY_FILTERS + ServiceStats.STAT_NAME_SUFFIX_PER_DAY);
+            return !(activeQueryStat == null || activeQueryStat.latestValue < 1.0);
+        });
+
+        // create serviceCount instances of SamplePreviousEchoService
+        // and save the links to these services
+        List<String> samplePreviousEchoServicesLinks;
+        List<Operation> opList = new ArrayList<>(this.serviceCount);
+        EchoServiceState state = new EchoServiceState();
+        state.message = "hello";
+
+        for (int i = 0; i < this.serviceCount; i++) {
+            opList.add(Operation.createPost(this.host, SamplePreviousEchoService.FACTORY_LINK).setBody(state));
+        }
+
+        samplePreviousEchoServicesLinks = this.sender.sendAndWait(
+                opList, EchoServiceState.class).stream().map(s -> s.documentSelfLink).collect(toList());
+
+        // get the continuous query watch service state and make sure that it was notified
+        // for the new SamplePreviousEchoService that we created
+        // the notification count should equal to the serviceCount
+        waitFor(this.timeout, () -> {
+            Operation getQueryWatchState = Operation.createGet(this.host, queryWatchSelfLink);
+            SampleContinuousQueryWatchService.State updatedWatchState =
+                    this.sender.sendAndWait(getQueryWatchState, SampleContinuousQueryWatchService.State.class);
+            this.host.log("notification count: %d", updatedWatchState.notificationsCounter);
+            return (this.serviceCount == updatedWatchState.notificationsCounter);
+        });
+
+        // update the state of all the SamplePreviousEchoService we created
+        final EchoServiceState updatedState = new EchoServiceState();
+        updatedState.message = "hello world";
+        this.host.testStart(samplePreviousEchoServicesLinks.size());
+        samplePreviousEchoServicesLinks.forEach(selfLink -> {
+            Operation.createPut(this.host, selfLink)
+                    .setBody(updatedState)
+                    .setCompletion(this.host.getCompletion()).sendWith(this.sender);
+        });
+        this.host.testWait();
+
+        // get the continuous query watch service state and make sure that it was notified
+        // for the updates to the SamplePreviousEchoService services
+        // the notification count should equal to serviceCount*2
+        // (serviceCount create operations + serviceCount put operations)
+        waitFor(this.timeout, () -> {
+            Operation getQueryWatchState = Operation.createGet(this.host, queryWatchSelfLink);
+            SampleContinuousQueryWatchService.State updatedQueryWatchState =
+                    this.sender.sendAndWait(getQueryWatchState, SampleContinuousQueryWatchService.State.class);
+            this.host.log("notification count: %d", updatedQueryWatchState.notificationsCounter);
+            return (this.serviceCount * 2 == updatedQueryWatchState.notificationsCounter);
+        });
+
+        // delete all the services
+        this.host.testStart(samplePreviousEchoServicesLinks.size());
+        samplePreviousEchoServicesLinks.forEach(selfLink -> {
+            Operation.createDelete(this.host, selfLink).setCompletion(this.host.getCompletion()).sendWith(this.sender);
+        });
+        this.host.testWait();
+
+        /// get the continuous query watch service state and make sure that it was notified
+        // for the deletes to the SamplePreviousEchoService services
+        // the notification count should equal to serviceCount*3
+        // (serviceCount create operations + serviceCount put operations +
+        //  serviceCount delete operations)
+        waitFor(this.timeout, () -> {
+            Operation getQueryWatchState = Operation.createGet(this.host, queryWatchSelfLink);
+            SampleContinuousQueryWatchService.State updatedQueryWatchState =
+                    this.sender.sendAndWait(getQueryWatchState, SampleContinuousQueryWatchService.State.class);
+            this.host.log("notification count: %d", updatedQueryWatchState.notificationsCounter);
+            return (this.serviceCount * 3 == updatedQueryWatchState.notificationsCounter);
+        });
+    }
+}
