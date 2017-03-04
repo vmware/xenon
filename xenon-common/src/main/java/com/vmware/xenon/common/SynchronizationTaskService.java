35fecf5d2d89484aad0bd062ad35ca54f2c92540
Touseef Liaqat <tliaqat@vmware.com>
2017-03-03 17:46:30 -0800

    Retry on failures in SynchronizationTaskService
    There are four ways a SynchronizationTaskService could go in FAILED state
     1. Query failure
     2. Page retrieval failure
     4. Owner selection failure
     5. Setting availability of FactoryService failure
    On each kind of these failures, a scheduled task is created
    to retry that operation. Stats are added to track these retries.
    Retries are done based on exponential backoff algorithm and
    after failure of last retry we set the FAILED state.
    
    Change-Id: Id4e08f3a67c05c3b9d574ad74b2b3f8d673bbb71

--- xenon-common/src/main/java/com/vmware/xenon/common/SynchronizationTaskService.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/SynchronizationTaskService.java
@@ -32,6 +32,16 @@ import com.vmware.xenon.services.common.TaskService;
 public class SynchronizationTaskService
         extends TaskService<SynchronizationTaskService.State> {
 
+
+    public static final String PROPERTY_NAME_MAX_SYNCH_RETRY_COUNT =
+            Utils.PROPERTY_NAME_PREFIX + "SynchronizationTaskService.MAX_SYNCH_RETRY_COUNT";
+
+    // Maximum synch-task retry limit.
+    // We are using exponential backoff for synchronization retry, that means last synch retry will
+    // be tried after 2 ^ 8 * getMaintenanceIntervalMicros(), which is ~4 minutes if maintenance interval is 1 second.
+    public static final int MAX_SYNCH_RETRY_COUNT = Integer.getInteger(
+            PROPERTY_NAME_MAX_SYNCH_RETRY_COUNT, 8);
+
     public static final String FACTORY_LINK = ServiceUriPaths.SYNCHRONIZATION_TASKS;
     public static final String PROPERTY_NAME_SYNCHRONIZATION_LOGGING = Utils.PROPERTY_NAME_PREFIX
             + "SynchronizationTaskService.isDetailedLoggingEnabled";
@@ -106,6 +116,7 @@ public class SynchronizationTaskService
     public SynchronizationTaskService() {
         super(State.class);
         toggleOption(ServiceOption.IDEMPOTENT_POST, true);
+        toggleOption(ServiceOption.INSTRUMENTATION, true);
     }
 
     /**
@@ -421,10 +432,16 @@ public class SynchronizationTaskService
                         if (!getHost().isStopping()) {
                             logWarning("Query failed with %s", e.toString());
                         }
-                        sendSelfFailurePatch(task, e.getMessage());
+
+                        scheduleTaskRetryOnFailure(
+                                () -> handleQueryStage(task),
+                                () -> sendSelfFailurePatch(task, e.getMessage()),
+                                STAT_NAME_QUERY_RETRY_COUNT,
+                                STAT_NAME_QUERY_FAILURE_COUNT);
                         return;
                     }
 
+                    setStat(STAT_NAME_QUERY_RETRY_COUNT, 0);
                     ServiceDocumentQueryResult rsp = o.getBody(QueryTask.class).results;
 
                     // Query returned zero results.Self-patch the task
@@ -442,6 +459,50 @@ public class SynchronizationTaskService
                 });
 
         sendRequest(queryPost);
+    }
+
+    private void scheduleTaskRetryOnFailure(
+            Runnable task, Runnable failure, String retryCountStatName, String failureCounterStatName) {
+
+        adjustStat(retryCountStatName, 1);
+        ServiceStats.ServiceStat stat = getStat(retryCountStatName);
+        if (stat != null) {
+            if (stat.latestValue > MAX_SYNCH_RETRY_COUNT) {
+                adjustStat(failureCounterStatName, 1);
+                logSevere("Retry counter (%s), exhausted maximum %d retires",
+                        retryCountStatName, (long) stat.latestValue);
+                failure.run();
+                return;
+            }
+        }
+
+        // Use exponential backoff algorithm in retry logic. The idea is to exponentially
+        // increase the delay for each retry based on the number of previous retries.
+        // This is done to reduce the load of retries on the system by all the synch tasks
+        // of all factories at the same time, and giving system more time to stabilize
+        // in next retry then the previous retry.
+        long delay = getExponentialDelay();
+
+        logWarning("Scheduling retry of task with counter %s:%d in %d seconds",
+                retryCountStatName, stat != null ? (int)stat.latestValue : 0, TimeUnit.MICROSECONDS.toSeconds(delay));
+        getHost().schedule(() -> task.run(),
+                delay, TimeUnit.MICROSECONDS);
+    }
+
+    /**
+     * Exponential backoff rely on retry count stat. If this stat is not available
+     * then we will fall back to constant delay for each retry.
+     * To get exponential delay, multiply retry count's power of 2 with constant delay.
+     */
+    private long getExponentialDelay() {
+        long delay = getHost().getMaintenanceIntervalMicros();
+
+        ServiceStats.ServiceStat stat = getStat(STAT_NAME_QUERY_RETRY_COUNT);
+        if (stat != null && stat.latestValue >= 0) {
+            return (1 << ((long)stat.latestValue)) * delay;
+        }
+
+        return delay;
     }
 
     private QueryTask buildChildQueryTask(State task) {
@@ -508,11 +569,17 @@ public class SynchronizationTaskService
                             task.queryPageReference,
                             e.toString());
                 }
-                sendSelfFailurePatch(task,
-                        "failure retrieving query page results");
+
+                scheduleTaskRetryOnFailure(
+                        () -> handleSynchronizeStage(task, verifyOwnership),
+                        () -> sendSelfFailurePatch(task, "failure retrieving query page results"),
+                        STAT_NAME_PAGE_RETRIEVAL_RETRY_COUNT,
+                        STAT_NAME_PAGE_RETRIEVAL_FAILURE_COUNT);
+
                 return;
             }
 
+            setStat(STAT_NAME_PAGE_RETRIEVAL_RETRY_COUNT, 0);
             ServiceDocumentQueryResult rsp = o.getBody(QueryTask.class).results;
             if (rsp.documentCount == 0 || rsp.documentLinks.isEmpty()) {
                 sendSelfFinishedPatch(task);
@@ -583,10 +650,16 @@ public class SynchronizationTaskService
                 .setExpiration(task.documentExpirationTimeMicros)
                 .setCompletion((o, e) -> {
                     if (e != null) {
-                        sendSelfFailurePatch(task, e.getMessage());
+                        scheduleTaskRetryOnFailure(
+                                () -> verifySynchronizationOwnership(task),
+                                () -> sendSelfFailurePatch(task, e.getMessage()),
+                                STAT_NAME_OWNER_SELECTION_RETRY_COUNT,
+                                STAT_NAME_OWNER_SELECTION_FAILURE_COUNT);
+
                         return;
                     }
 
+                    setStat(STAT_NAME_OWNER_SELECTION_RETRY_COUNT, 0);
                     NodeSelectorService.SelectOwnerResponse rsp = o.getBody(
                             NodeSelectorService.SelectOwnerResponse.class);
 
@@ -652,9 +725,17 @@ public class SynchronizationTaskService
                     }
                     if (e != null) {
                         logSevere("Setting factory availability failed with error %s", e.getMessage());
-                        sendSelfFailurePatch(task, "Failed to set Factory Availability");
+                        scheduleTaskRetryOnFailure(
+                                () -> setFactoryAvailability(task, isAvailable, action, parentOp),
+                                () -> sendSelfFailurePatch(task, "Failed to set Factory Availability"),
+                                STAT_NAME_SET_AVAILABILITY_RETRY_COUNT,
+                                STAT_NAME_SET_AVAILABILITY_FAILURE_COUNT);
+
                         return;
                     }
+
+                    setStat(STAT_NAME_SET_AVAILABILITY_RETRY_COUNT, 0);
+
                     if (action != null) {
                         action.accept(o);
                     }
