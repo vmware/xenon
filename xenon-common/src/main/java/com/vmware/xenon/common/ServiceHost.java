3a8f5b4b5e36cd06b9acb2c916e505e10d473cb3
Kenneth Jung <jungk@vmware.com>
2017-03-13 12:17:09 -0700

    Gateway: Don't allocate default core service instances.
    The GatewayDispatchHost does not start default core services.
    However, it still allocates instances of some of these services at
    initialization time. This change forces these allocations to be
    discarded.
    
    Change-Id: I2023a7079de7df2a065d03f64a11c923425a8e88

--- xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
@@ -5028,15 +5028,17 @@ public class ServiceHost implements ServiceRequestSender {
             }
 
             if (stage == null) {
-                // update the maintenance count stat for the ServiceHost before, completing
-                // the current maintenance run.
-                this.managementService.adjustStat(
-                        Service.STAT_NAME_SERVICE_HOST_MAINTENANCE_COUNT, 1);
+                if (this.managementService != null) {
+                    // update the maintenance count stat for the ServiceHost before, completing
+                    // the current maintenance run.
+                    this.managementService.adjustStat(
+                            Service.STAT_NAME_SERVICE_HOST_MAINTENANCE_COUNT, 1);
 
-                // Update the count of services that are pending delete on the service host.
-                this.managementService.setStat(
-                        ServiceHostManagementService.STAT_NAME_PENDING_SERVICE_DELETION_COUNT,
-                        this.pendingServiceDeletions.size());
+                    // Update the count of services that are pending delete on the service host.
+                    this.managementService.setStat(
+                            ServiceHostManagementService.STAT_NAME_PENDING_SERVICE_DELETION_COUNT,
+                            this.pendingServiceDeletions.size());
+                }
 
                 post.complete();
                 scheduleMaintenance();
@@ -5044,7 +5046,7 @@ public class ServiceHost implements ServiceRequestSender {
             }
             performMaintenanceStage(post, stage, deadline);
         } catch (Throwable e) {
-            log(Level.SEVERE, "Uncaught exception: %s", e.toString());
+            log(Level.SEVERE, "Uncaught exception: %s", Utils.toString(e));
             post.fail(e);
         }
     }
