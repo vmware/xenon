6bd2b2dd281cd994fa9b1b03b3972beeb19a776b
Tyler Curtis <tcurtis@vmware.com>
2017-04-19 09:29:54 -0700

    [WIP] Attempt to repro QueryTask IllegalStateException
    NOTE: This is not meant to commit; I'm trying to reproduce a problem I'm
    having in symphony.
    
    I'm noticing when I run a QueryTask with:
    - documentKind clause
    - sort clause with fieldName that matches other service document field
      names (i.e.: name)
    - there are zero documents created matching documentKind
    
    ... that the QueryTask intermittently fails with the following error:
    
    Exception on index service thread: java.lang.IllegalStateException:
    unexpected docvalues type NONE for field 'name' (expected=SORTED). Use
    UninvertingReader or index with docvalues.
    
    It looks like this happens when no DocValues value has been created for
    the specified sort term.
    
    I was hoping this changeset would reproduce the problem... but it
    hasn't. :/ My 'symphony' review does reproduce the problem though.
    
    - http://bellevue-ci.eng.vmware.com:8080/#/c/9238/
    - TestEndpointApiService.testCloudAccountListNoEndpoints()
    
    Change-Id: I0e41a38e3828589d21ae8bfd06406b33be57aad0

---
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/ExampleBarService.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2014-2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.services.common;
+
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.ServiceDocumentDescription;
+import com.vmware.xenon.common.ServiceDocumentDescription.PropertyIndexingOption;
+import com.vmware.xenon.common.ServiceDocumentDescription.PropertyUsageOption;
+import com.vmware.xenon.common.StatefulService;
+
+public class ExampleBarService extends StatefulService {
+    public static final String FACTORY_LINK = ServiceUriPaths.CORE + "/bar";
+
+    public static class ExampleBarState extends ServiceDocument {
+        public static final String FIELD_NAME_NAME = "name";
+
+        @UsageOption(option = PropertyUsageOption.AUTO_MERGE_IF_NOT_NULL)
+        @PropertyOptions(indexing = { PropertyIndexingOption.CASE_INSENSITIVE, PropertyIndexingOption.SORT })
+        public String name;
+
+        @UsageOption(option = ServiceDocumentDescription.PropertyUsageOption.AUTO_MERGE_IF_NOT_NULL)
+        @PropertyOptions(indexing = ServiceDocumentDescription.PropertyIndexingOption.SORT)
+        public String barField;
+    }
+
+    public ExampleBarService() {
+        super(ExampleBarState.class);
+        super.toggleOption(ServiceOption.PERSISTENCE, true);
+        super.toggleOption(ServiceOption.REPLICATION, true);
+        super.toggleOption(ServiceOption.OWNER_SELECTION, true);
+        super.toggleOption(ServiceOption.IDEMPOTENT_POST, true);
+    }
+}
---
+++ xenon-common/src/test/java/com/vmware/xenon/services/common/TestMultiNodeQueryTask.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright (c) 2014-2017 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.services.common;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.net.URI;
+import java.util.UUID;
+import java.util.logging.Level;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vmware.xenon.common.BasicTestCase;
+import com.vmware.xenon.common.CommandLineArgumentParser;
+import com.vmware.xenon.common.ServiceDocument;
+import com.vmware.xenon.common.ServiceDocumentDescription;
+import com.vmware.xenon.common.UriUtils;
+import com.vmware.xenon.services.common.QueryTask;
+import com.vmware.xenon.services.common.QueryTask.Query;
+import com.vmware.xenon.services.common.QueryTask.QuerySpecification.QueryOption;
+import com.vmware.xenon.common.Utils;
+import com.vmware.xenon.common.test.VerificationHost;
+
+public class TestMultiNodeQueryTask extends BasicTestCase {
+    public int numNodes = 3;
+
+    private URI peerUri;
+
+    @Before
+    public void setUp() throws Throwable {
+        CommandLineArgumentParser.parseFromProperties(this);
+
+        host.setUpPeerHosts(numNodes);
+        host.joinNodesAndVerifyConvergence(numNodes, numNodes, true);
+        host.setNodeGroupQuorum(numNodes);
+        this.peerUri = this.host.getPeerHostUri();
+
+        for (VerificationHost h : host.getInProcessHostMap().values()) {
+            h.startFactory(new ExampleFooService());
+            h.startFactory(new ExampleBarService());
+        }
+        host.waitForReplicatedFactoryServiceAvailable(UriUtils.buildUri(this.peerUri, ExampleFooService.FACTORY_LINK));
+        host.waitForReplicatedFactoryServiceAvailable(UriUtils.buildUri(this.peerUri, ExampleBarService.FACTORY_LINK));
+    }
+
+    @After
+    public void tearDown() throws Throwable {
+        if (this.host == null) {
+            return;
+        }
+        this.host.tearDownInProcessPeers();
+        this.host.toggleNegativeTestMode(false);
+        this.host.tearDown();
+    }
+
+    @Test
+    public void sortOnSharedNameNoResults() throws Throwable {
+        Query kindClause = Query.Builder.create()
+                .addKindFieldClause(ExampleService.ExampleServiceState.class)
+                .build();
+
+        QueryTask task = QueryTask.Builder.createDirectTask()
+                .setQuery(kindClause)
+                .addOption(QueryOption.EXPAND_CONTENT)
+                .orderAscending("name", ServiceDocumentDescription.TypeName.STRING)
+                .setResultLimit(50)
+                .build();
+
+        URI queryTaskURI = UriUtils.buildUri(this.peerUri, ServiceUriPaths.CORE_LOCAL_QUERY_TASKS);
+        this.host.log(Level.INFO, Utils.toJson(task));
+        this.host.createQueryTaskService(queryTaskURI, task, false,true, task, null);
+        assertNotNull(task.results);
+        assertEquals(Long.valueOf(0), task.results.documentCount);
+    }
+
+}
