5fd3bdfa12e4e34c7da67cbdbcf7993a22f60cbf
Jonida Cali <calij@vmware.com>
2017-07-07 09:50:44 -0700

    Handle NullPointerException in QueryTaskUtils
    In addition if any issue occurs with the exapndLinks, mark
    the task as failed so it propagates to the top operation
    
    Change-Id: I0c20961e3a5e95fbd33266f1be1c03eba6788d4a

--- xenon-common/src/main/java/com/vmware/xenon/services/common/QueryPageService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/QueryPageService.java
@@ -178,7 +178,16 @@ public class QueryPageService extends StatelessService {
         QueryRuntimeContext context = task.querySpec.context;
         task.querySpec.context = null;
         task.taskInfo.stage = TaskStage.FINISHED;
-        QueryTaskUtils.expandLinks(getHost(), task, get);
+        try {
+            QueryTaskUtils.expandLinks(getHost(), task, get);
+        } catch (Exception ex) {
+            // fail the paginated query, client has to re-create the query task
+            QueryTask t = new QueryTask();
+            t.taskInfo.stage = TaskStage.FAILED;
+            t.taskInfo.failure = Utils.toServiceErrorResponse(ex);
+            get.setBody(t).fail(ex);
+            return;
+        }
 
         if (singleUse && task.results.nextPageLink == null) {
             DeleteQueryRuntimeContextRequest request = new DeleteQueryRuntimeContextRequest();
