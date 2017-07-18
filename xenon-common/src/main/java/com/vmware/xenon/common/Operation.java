8814cd3c8d241d27037c0832182581b7272de74a
Li Yu <lyu1@vmware.com>
2017-07-18 15:07:39 -0700

    WIP: http1/2 timeout operation with status code 200
    when server response and timeout happen at the same time
    racing in channel conext stream/operation
    
    Change-Id: I9d18cee91a98d4ed1e2ce89d5067b5995922ba77

--- xenon-common/src/main/java/com/vmware/xenon/common/Operation.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/Operation.java
@@ -23,6 +23,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.function.Consumer;
@@ -932,8 +933,10 @@ public class Operation implements Cloneable {
     }
 
     public Operation setStatusCode(int code) {
-        this.statusCode = code;
-        return this;
+        synchronized (this) {
+            this.statusCode = code;
+            return this;
+        }
     }
 
     /**
@@ -1212,7 +1215,9 @@ public class Operation implements Cloneable {
     }
 
     public int getStatusCode() {
-        return this.statusCode;
+        synchronized (this) {
+            return this.statusCode;
+        }
     }
 
     public void complete() {
