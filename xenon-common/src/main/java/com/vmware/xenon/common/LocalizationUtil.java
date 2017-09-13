3324b965629fcf6adc2fcd6cbac52f7e37ae0162
Stanislav Hadjiiski <hadjiiskis@vmware.com>
2017-09-13 18:06:00 +0300

    Use the correct encoding when reading localized resource bundles
    Resource bundles (e.g. for localized error messages) were loaded with
    the ISO-8859-1 encoding. This was due to the default behavior of the
    java.util.ResourceBundle.getBundle(filename, locale) method. After this
    commit bundles will be loaded with UTF-8 encoding as well to provide
    support for languages that are not compatible with ISO-8859-1.
    
    Change-Id: I25c7a3846ba56f007beebc524d530f3d1b0036ac

--- xenon-common/src/main/java/com/vmware/xenon/common/LocalizationUtil.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/LocalizationUtil.java
@@ -13,10 +13,20 @@
 
 package com.vmware.xenon.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.text.MessageFormat;
 import java.util.List;
 import java.util.Locale;
 import java.util.MissingResourceException;
+import java.util.PropertyResourceBundle;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -54,11 +64,13 @@ public class LocalizationUtil {
     private static String resolveMessage(LocalizableValidationException e, Locale locale) {
 
         if (locale == null) {
-            Logger.getAnonymousLogger().fine("Locale not provided for localization, using default locale.");
+            Logger.getAnonymousLogger()
+                    .fine("Locale not provided for localization, using default locale.");
             locale = DEFAULT_LOCALE;
         }
 
-        ResourceBundle messages = ResourceBundle.getBundle(MESSAGES_BASE_FILENAME, locale);
+        ResourceBundle messages = ResourceBundle.getBundle(MESSAGES_BASE_FILENAME, locale,
+                new Utf8LoaderControl());
 
         String message;
         try {
@@ -91,5 +103,93 @@ public class LocalizationUtil {
         }
 
         return DEFAULT_LOCALE;
+    }
+
+    /**
+     * This is the default Control implementation with the only difference that UTF-8 is used to
+     * load the resource bundles instead of ISO-8859-1. This is needed since some resource bundles
+     * contain messages that are not ISO-8859-1 compatible and therefore are wrongly displayed.
+     */
+    private static class Utf8LoaderControl extends ResourceBundle.Control {
+        @Override
+        public ResourceBundle newBundle(String baseName, Locale locale, String format,
+                ClassLoader loader, boolean reload)
+                throws IllegalAccessException, InstantiationException, IOException {
+            String bundleName = toBundleName(baseName, locale);
+            ResourceBundle bundle = null;
+            if (format.equals("java.class")) {
+                try {
+                    @SuppressWarnings("unchecked")
+                    Class<? extends ResourceBundle> bundleClass = (Class<? extends ResourceBundle>) loader
+                            .loadClass(bundleName);
+
+                    // If the class isn't a ResourceBundle subclass, throw a
+                    // ClassCastException.
+                    if (ResourceBundle.class.isAssignableFrom(bundleClass)) {
+                        bundle = bundleClass.newInstance();
+                    } else {
+                        throw new ClassCastException(bundleClass.getName()
+                                + " cannot be cast to ResourceBundle");
+                    }
+                } catch (ClassNotFoundException e) {
+                }
+            } else if (format.equals("java.properties")) {
+                final String resourceName = toResourceName0(bundleName, "properties");
+                if (resourceName == null) {
+                    return bundle;
+                }
+                final ClassLoader classLoader = loader;
+                final boolean reloadFlag = reload;
+                InputStream stream = null;
+                try {
+                    stream = AccessController.doPrivileged(
+                            new PrivilegedExceptionAction<InputStream>() {
+                                public InputStream run() throws IOException {
+                                    InputStream is = null;
+                                    if (reloadFlag) {
+                                        URL url = classLoader.getResource(resourceName);
+                                        if (url != null) {
+                                            URLConnection connection = url.openConnection();
+                                            if (connection != null) {
+                                                // Disable caches to get fresh data for
+                                                // reloading.
+                                                connection.setUseCaches(false);
+                                                is = connection.getInputStream();
+                                            }
+                                        }
+                                    } else {
+                                        is = classLoader.getResourceAsStream(resourceName);
+                                    }
+                                    return is;
+                                }
+                            });
+                } catch (PrivilegedActionException e) {
+                    throw (IOException) e.getException();
+                }
+                if (stream != null) {
+                    try {
+                        // Wrap the stream in a reader in order to change the default character
+                        // encoding (ISO-8859-1) to UTF-8
+                        bundle = new PropertyResourceBundle(
+                                new InputStreamReader(stream, StandardCharsets.UTF_8));
+                    } finally {
+                        stream.close();
+                    }
+                }
+            } else {
+                throw new IllegalArgumentException("unknown format: " + format);
+            }
+            return bundle;
+        }
+
+        // copy of the original method which is not visible due to access modifiers
+        private String toResourceName0(String bundleName, String suffix) {
+            // application protocol check
+            if (bundleName.contains("://")) {
+                return null;
+            } else {
+                return toResourceName(bundleName, suffix);
+            }
+        }
     }
 }
