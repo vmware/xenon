5e6835bfb75e2a79ef4ca88b021fde809cc8b16b
Li Yu <lyu1@vmware.com>
2017-09-14 14:45:22 -0700

    WIP Node restart with same ip address and different id
    When node restart with same ip and new id. Node will
    receive probing patch with entry share the ip and old id.
    Though peer will mark that entry as replaced, node itself
    should also decline update. If node accept that old entry
    as available, then request to that node might be looped
    to itself given that old id is selected.
    
    https://www.pivotaltracker.com/story/show/150535341
    
    Change-Id: I4e405b732042aaeae6a712079de07bfe6c92e575

--- xenon-common/src/test/java/com/vmware/xenon/common/TestSynchronizationTaskService.java
+++ xenon-common/src/test/java/com/vmware/xenon/common/TestSynchronizationTaskService.java
@@ -39,7 +39,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import com.vmware.xenon.common.test.TestContext;
@@ -406,12 +405,11 @@ public class TestSynchronizationTaskService extends BasicTestCase {
         return newState;
     }
 
-    @Ignore("https://www.pivotaltracker.com/story/show/150535341")
     @Test
     public void  synchAfterClusterRestart() throws Throwable {
         setUpMultiNode();
         String factoryLink = ExampleService.FACTORY_LINK;
-        this.host.setNodeGroupQuorum(this.nodeCount - 1);
+        this.host.setNodeGroupQuorum(this.nodeCount);
         this.host.waitForNodeGroupConvergence();
 
         List<ExampleServiceState> exampleStates = this.host.createExampleServices(
@@ -450,6 +448,7 @@ public class TestSynchronizationTaskService extends BasicTestCase {
         this.host.joinNodesAndVerifyConvergence(this.nodeCount);
         this.host.waitForNodeGroupConvergence(this.nodeCount, this.nodeCount);
 
+
         // Verify that all states are replicated and synched.
         this.host.waitForReplicatedFactoryChildServiceConvergence(
                 this.host.getNodeGroupToFactoryMap(factoryLink),
@@ -467,6 +466,10 @@ public class TestSynchronizationTaskService extends BasicTestCase {
                 VerificationHost.FAST_MAINT_INTERVAL_MILLIS, null, null);
         this.host.joinNodesAndVerifyConvergence(this.nodeCount);
         this.host.waitForNodeGroupConvergence(this.nodeCount, this.nodeCount);
+
+        URI exampleFactoryUri = UriUtils.buildUri(
+                this.host.getPeerServiceUri(ExampleService.FACTORY_LINK));
+        this.host.waitForReplicatedFactoryServiceAvailable(exampleFactoryUri);
 
         // Verify that all states are replicated and synched.
         this.host.waitForReplicatedFactoryChildServiceConvergence(
