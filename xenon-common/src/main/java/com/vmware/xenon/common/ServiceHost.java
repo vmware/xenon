af2993b258b49e439e9c32cbccf4c20708842409
Tadaya Tsuyukubo <ttsuyukubo@vmware.com>
2017-10-04 11:51:09 -0700

    Reduce coupling between index-service and backup-service
    Previously, backup-service had constructor dependency to index-service.
    This is against xenon programming model - service should be dependent by
    link.
    
    In this change, constructor dependency is removed, and index-service adds a
    special internal-only GET request that returns lucene related object
    required to take backup/restore.  With this change, non built-in backup
    service that co-locate to the node can retrieve lucene object and perform
    backup with own logic.
    
    Also, backup/restore request now takes "indexServiceLink"
    ("backupServiceLink" already exists) to allow caller to specify target index
    and backup services independently.  This way, caller can freely choose
    index-service and backup/restore service based on its strategy.
    
    Note: Restore logic still requires index-service in order to call some
    package scoped methods.  This is future TODO for building a custom restore
    service.
    
    Some design discussion can be found this github comment:
    https://github.com/vmware/xenon/commit/bb9d514f285ec833d4b39935c6fe450a44bb6205
    
    https://www.pivotaltracker.com/story/show/151526649
    
    Change-Id: I8eab7519fc53144209adce5e6328f017e94a2443

--- xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
+++ xenon-common/src/main/java/com/vmware/xenon/common/ServiceHost.java
@@ -1603,7 +1603,7 @@ public class ServiceHost implements ServiceRequestSender {
                 LuceneDocumentIndexService luceneDocumentIndexService = (LuceneDocumentIndexService) this.documentIndexService;
                 Service[] queryServiceArray = new Service[] {
                         luceneDocumentIndexService,
-                        new LuceneDocumentIndexBackupService(luceneDocumentIndexService),
+                        new LuceneDocumentIndexBackupService(),
                         new QueryTaskFactoryService(),
                         new LocalQueryTaskFactoryService(),
                         TaskFactoryService.create(GraphQueryTaskService.class),
