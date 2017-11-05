d901e796c64c2499eb319cb11c4ef4d87fbfe341
Julian Vassev <jvassev@vmware.com>
2017-11-05 21:06:31 +0200

    Streamline Xenon configuration read
    This change aims to do this:
    * introduce a single entrypoint to access global config params from the
       environemnt
    
    * Promote convention when introducing config params in the form of
      InterestedClass.propName. All configs that don't adhere to this convention
      are still read the old way.
    
    * Enable reading configs from both sysprops and env vars: these two are
      equivalent:
      -Dxenon.MyConfigs.key=value
      export XENON_MY_CONFIG_KEY=value
    
      This makes it easier to parameterize xenon when run in container/pod.
      Inspired by https://github.com/kelseyhightower/envconfig.
    
    * User friendliness: durations can be given in millis, seconds,
      minutes, hours, days; micros are the default. These are equivalent:
      1h = 3600s = 3600000ms = 3600000000
    
    * User friendliness: number parameters understand K,M,G suffixes. These
      are all equivalent:
      1M = 1m = 1024K = 1024k = ....
    
    * Standadrd API to modify/restore configuration for test
    
    * Clean-up constants used only in a scope of a few line (the property names and
      defaults)
    
    * Debuggability: during host start, all properties read log why they get this
      value and wheather the default is used (inspired by Java's  -XX:+PrintFlagsFinal)
    
    All places when a config is read are changed to use the new API. This API is
    not considered public yet. It may be advertised as such once stable enough.
    
    https://www.pivotaltracker.com/story/show/151027195
    
    Change-Id: I38d85dcaa25df3d12eeabf5fdc8a6b9cf63449f1

---
+++ xenon-common/src/main/java/com/vmware/xenon/common/config/XenonConfiguration.java
@@ -0,0 +1,308 @@
+/*
+ * Copyright (c) 2017 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.common.config;
+
+import java.time.Duration;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Reads configuration from environment. First system properties are examined then ENV variables.
+ * To configure the property "maxTTL" of an imaginary class SystemLimits you have to pass either of these:
+ * -Dxenon.SystemLimits.maxTTL=250
+ * export XENON_SYSTEM_LIMITS_MAX_TTL=250
+ */
+public final class XenonConfiguration {
+    private final ConfigurationSource source;
+
+    static final Logger logger = Logger.getLogger(XenonConfiguration.class.getName());
+
+    private static final XenonConfiguration INSTANCE = new XenonConfiguration(new DefaultSource());
+
+    XenonConfiguration(ConfigurationSource source) {
+        this.source = source;
+    }
+
+    /**
+     * Returns a double-precision number.
+     * @param subsystem
+     * @param property
+     * @param defaultValue
+     * @return
+     */
+    public static double decimal(Class<?> subsystem, String property, double defaultValue) {
+        return INSTANCE.getDecimal(subsystem, property, defaultValue);
+    }
+
+    double getDecimal(Class<?> subsystem, String property, double defaultValue) {
+        String v = this.source.get(subsystem, property);
+
+        if (v == null) {
+            logUsingDefaulBecausetUnset(subsystem, property, defaultValue);
+            return defaultValue;
+        }
+
+        try {
+            return Double.parseDouble(v);
+        } catch (NumberFormatException e) {
+            logUsingDefaultBecauseBadValue(subsystem, property, defaultValue, v);
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Returns a number, can be negative.
+     * @param subsystem
+     * @param property
+     * @param defaultValue
+     * @return
+     */
+    public static long integer(Class<?> subsystem, String property, long defaultValue) {
+        return INSTANCE.getInteger(subsystem, property, defaultValue);
+    }
+
+    long getInteger(Class<?> subsystem, String property, long defaultValue) {
+        String v = this.source.get(subsystem, property);
+
+        if (v == null) {
+            logUsingDefaulBecausetUnset(subsystem, property, defaultValue);
+            return defaultValue;
+        }
+
+        try {
+            return Long.parseLong(v);
+        } catch (NumberFormatException e) {
+        }
+
+        if (lastToUpper(v) == 'K') {
+            Long res = parseOr(stripSuffix(v, 1));
+            if (res == null) {
+                logUsingDefaultBecauseBadValue(subsystem, property, defaultValue, v);
+                return defaultValue;
+            } else {
+                return 1024 * res;
+            }
+        }
+        if (lastToUpper(v) == 'M') {
+            Long res = parseOr(stripSuffix(v, 1));
+            if (res == null) {
+                logUsingDefaultBecauseBadValue(subsystem, property, defaultValue, v);
+                return defaultValue;
+            } else {
+                return 1024 * 1024 * res;
+            }
+        }
+        if (lastToUpper(v) == 'G') {
+            Long res = parseOr(stripSuffix(v, 1));
+            if (res == null) {
+                logUsingDefaultBecauseBadValue(subsystem, property, defaultValue, v);
+                return defaultValue;
+            } else {
+                return 1024 * 1024 * 1024 * res;
+            }
+        }
+
+        logUsingDefaultBecauseBadValue(subsystem, property, defaultValue, v);
+        return defaultValue;
+    }
+
+    private char lastToUpper(String v) {
+        return Character.toUpperCase(v.charAt(v.length() - 1));
+    }
+
+    private void logUsingDefaulBecausetUnset(Object subsystem, String property, Object defaultValue) {
+        if (logger.isLoggable(Level.FINE)) {
+            logger.log(Level.FINE,
+                    String.format("Using default value '%s' for %s.%s",
+                            defaultValue,
+                            subsystem,
+                            property));
+        }
+    }
+
+    private void logUsingDefaultBecauseBadValue(Class<?> subsystem, String property, Object defaultValue,
+            String badValue) {
+        if (logger.isLoggable(Level.FINE)) {
+            logger.log(Level.FINE,
+                    String.format("Using default value '%s' for %s.%s. Cannot parse %s",
+                            defaultValue,
+                            subsystem.getSimpleName(),
+                            property, badValue));
+        }
+    }
+
+    private Long parseOr(String v) {
+        if (v == null) {
+            return null;
+        }
+        try {
+            return Long.parseLong(v);
+        } catch (NumberFormatException ignore) {
+            return null;
+        }
+    }
+
+    /**
+     * Return a number of microseconds. This methods understand these suffixes:
+     * ms for millis
+     * s for seconds
+     * h for hours
+     * d for days.
+     *
+     * No suffix means microseconds.
+     * @param subsystem
+     * @param property
+     * @param defaultValue
+     * @return
+     */
+    public static long duration(Class<?> subsystem, String property, Duration defaultValue) {
+        return INSTANCE.getDuration(subsystem, property, defaultValue);
+    }
+
+    /**
+     * see {@link #duration(Class, String, Duration)}
+     * @param subsystem
+     * @param property
+     * @param unit
+     * @param duration
+     * @return
+     */
+    public static long duration(Class<?> subsystem, String property, TimeUnit unit, long duration) {
+        return INSTANCE.getDuration(subsystem, property, Duration.ofNanos(unit.toNanos(duration)));
+    }
+
+    long getDuration(Class<?> subsystem, String property, Duration defaultValue) {
+        String v = this.source.get(subsystem, property);
+        long dv = defaultValue.toNanos() / 1000;
+
+        if (v == null) {
+            logUsingDefaulBecausetUnset(subsystem, property, dv);
+            return dv;
+        }
+
+        try {
+            long micros = Long.parseLong(v);
+            if (micros < 0) {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            } else {
+                return micros;
+            }
+        } catch (NumberFormatException ignore) {
+
+        }
+
+        if (v.endsWith("ms")) {
+            Long ms = parseOr(stripSuffix(v, 2));
+            if (ms != null && ms >= 0) {
+                return TimeUnit.MILLISECONDS.toMicros(ms);
+            } else {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            }
+        }
+
+        if (v.endsWith("h")) {
+            Long h = parseOr(stripSuffix(v, 1));
+            if (h != null && h >= 0) {
+                return TimeUnit.HOURS.toMicros(h);
+            } else {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            }
+        }
+
+        if (v.endsWith("d")) {
+            Long days = parseOr(stripSuffix(v, 1));
+            if (days != null && days >= 0) {
+                return TimeUnit.DAYS.toMicros(days);
+            } else {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            }
+        }
+
+        if (v.endsWith("s")) {
+            Long sec = parseOr(stripSuffix(v, 1));
+            if (sec != null && sec >= 0) {
+                return TimeUnit.SECONDS.toMicros(sec);
+            } else {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            }
+        }
+
+        if (v.endsWith("m")) {
+            Long min = parseOr(stripSuffix(v, 1));
+            if (min != null && min >= 0) {
+                return TimeUnit.MINUTES.toMicros(min);
+            } else {
+                logUsingDefaultBecauseBadValue(subsystem, property, dv, v);
+                return dv;
+            }
+        }
+
+        return dv;
+    }
+
+    private String stripSuffix(String v, int suffixLen) {
+        return v.substring(0, v.length() - suffixLen);
+    }
+
+    public static String string(Class<?> subsystem, String property, String defaultValue) {
+        return INSTANCE.getString(subsystem, property, defaultValue);
+    }
+
+    String getString(Class<?> subsystem, String property, String defaultValue) {
+        String v = this.source.get(subsystem, property);
+        if (v == null) {
+            return defaultValue;
+        } else {
+            return v;
+        }
+    }
+
+    public static boolean bool(String subsystem, String property, boolean defaultValue) {
+        return INSTANCE.getBool(subsystem, property, defaultValue);
+    }
+
+    public static boolean bool(Class<?> subsystem, String property, boolean defaultValue) {
+        return INSTANCE.getBool(subsystem, property, defaultValue);
+    }
+
+    boolean getBool(String subsystem, String property, boolean defaultValue) {
+        String v = this.source.get(subsystem, property);
+
+        if (v == null) {
+            logUsingDefaulBecausetUnset(subsystem, property, defaultValue);
+            return defaultValue;
+        }
+
+        // popular way of setting booleans in env vars
+        Long num = parseOr(v);
+        if (num != null && num == 0) {
+            return false;
+        } else if (num != null && num == 1) {
+            return true;
+        }
+
+        return Boolean.parseBoolean(v);
+    }
+
+    boolean getBool(Class<?> subsystem, String property, boolean defaultValue) {
+        return getBool(subsystem.getSimpleName(), property, defaultValue);
+    }
+}
