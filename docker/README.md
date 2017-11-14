5139fc2a3d425d7494f78a3cea7f01476639dae5
Li Yu <lyu1@vmware.com>
2017-11-13 18:12:19 -0800

    Unit test in container
    provide ContainerVerificationHost to deploy
    test nodes in containers.
    provide unified container logging in realtime
    
    see docker/README.md for how to run tests in container
    
    Change-Id: I6af6521e247269ee271a89304f67a95a22a58701

---
+++ docker/README.md
@@ -0,0 +1,6 @@
+# build the jar file and edit JAR and JAR_PATH in Makefile
+# build images
+make build
+# go back to xenon parent path
+# run test in container
+./mvnw -B -e package -DskipAnalysis -pl xenon-common -Dtest=RunTestInContainer -Dxenon.containerImage=vmware/xenon-base -Dxenon.containerEnv=JVM_HEAP=256m,JVM_METASPACE=100m -Dxenon.containerExposedPort=8000
---
+++ xenon-common/src/test/java/com/vmware/xenon/common/test/RunTestInContainer.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2014-2015 VMware, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.vmware.xenon.common.test;
+
+import java.net.URI;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.vmware.xenon.common.CommandLineArgumentParser;
+import com.vmware.xenon.common.Operation;
+import com.vmware.xenon.common.ServiceDocumentQueryResult;
+import com.vmware.xenon.common.UriUtils;
+import com.vmware.xenon.common.Utils;
+import com.vmware.xenon.services.common.ExampleService;
+import com.vmware.xenon.services.common.ServiceUriPaths;
+import com.vmware.xenon.services.common.SynchronizationManagementService;
+
+public class RunTestInContainer {
+
+    ContainerVerificationHost host;
+    // set long time out for container environment
+    private static final int DEFAULT_TEST_TIMEOUT_IN_SECOND = 600;
+    public String factoryLink = ExampleService.FACTORY_LINK;
+    public int nodeCount = 3;
+    public int serviceCount = 10;
+    public boolean longRun = true;
+
+    @Before
+    public void setUp() throws Throwable {
+        CommandLineArgumentParser.parseFromProperties(this);
+        // create verification host which create xenon node in docker container
+        this.host = ContainerVerificationHost.create(0);
+        if (this.longRun) {
+            this.host.setTimeoutSeconds(Integer.MAX_VALUE);
+        } else {
+            this.host.setTimeoutSeconds(DEFAULT_TEST_TIMEOUT_IN_SECOND);
+        }
+        this.host.start();
+    }
+
+    @Test
+    public void nodeLeftThenJoinSyncInContainer() throws Throwable {
+        this.host.setUpPeerHosts(this.nodeCount);
+        this.host.joinNodesAndVerifyConvergence(this.nodeCount);
+        this.host.setNodeGroupQuorum(this.nodeCount - 1);
+        // create more example services
+        this.host.createExampleServices(this.host.getPeerHost(), this.serviceCount, null, this.factoryLink);
+        ContainerVerificationHost h0 = this.host.getPeerHost();
+        // stop one node with index removed
+        this.host.stopHost(h0);
+        this.host.waitForNodeUnavailable(ServiceUriPaths.DEFAULT_NODE_GROUP, this.host.getInProcessHostMap().values(), h0);
+        this.host.waitForNodeGroupConvergence(this.host.getPeerCount());
+        waitForFactoryAvailable();
+        // create more examples
+        this.host.createExampleServices(this.host.getPeerHost(), this.serviceCount, null, this.factoryLink);
+        this.host.setUpPeerHosts(1);
+        this.host.joinNodesAndVerifyConvergence(this.nodeCount);
+        waitForFactoryAvailable();
+    }
+
+    @Test
+    public void nodeRestartSyncInContainer() throws Throwable {
+        this.host.setUpPeerHosts(this.nodeCount);
+        this.host.joinNodesAndVerifyConvergence(this.nodeCount);
+        this.host.setNodeGroupQuorum(this.nodeCount - 1);
+        //create services
+        this.host.createExampleServices(this.host.getPeerHost(), this.serviceCount, null, this.factoryLink);
+        ContainerVerificationHost h0 = this.host.getPeerHost();
+        // stop service preserve index
+        this.host.stopHostAndPreserveState(h0);
+        this.host.waitForNodeUnavailable(ServiceUriPaths.DEFAULT_NODE_GROUP, this.host.getInProcessHostMap().values(), h0);
+        this.host.waitForNodeGroupConvergence(this.host.getPeerCount());
+        waitForFactoryAvailable();
+        // create more services
+        this.host.createExampleServices(this.host.getPeerHost(), this.serviceCount, null, this.factoryLink);
+        // restart node with preserved index
+        this.host.resumeHostInContainer(h0);
+        this.host.joinNodesAndVerifyConvergence(this.nodeCount);
+        waitForFactoryAvailable();
+    }
+
+    public void waitForFactoryAvailable() {
+        this.host.waitFor("Factory availability timeout", () -> {
+            URI uri = UriUtils.buildUri(this.host.getPeerHost(), SynchronizationManagementService.class);
+            ServiceDocumentQueryResult result = null;
+            try {
+                result = this.host.getTestRequestSender().sendAndWait(Operation.createGet(uri), ServiceDocumentQueryResult.class);
+            } catch (RuntimeException e) {
+                // receive failed response
+                return false;
+            }
+            SynchronizationManagementService.SynchronizationManagementState state =
+                    Utils.fromJson(result.documents.get(this.factoryLink), SynchronizationManagementService.SynchronizationManagementState.class);
+            if (state.owner == null) {
+                return false;
+            }
+            if (!this.host.containPeerId(state.owner)) {
+                return false;
+            }
+            if (state.status != SynchronizationManagementService.SynchronizationManagementState.Status.AVAILABLE) {
+                return false;
+            }
+            return true;
+        });
+    }
+
+    @After
+    public void cleanUp() {
+        this.host.tearDown();
+        this.host.tearDownInProcessPeers();
+    }
+
+}
