193b155694764dd576acd4de92c6e09edb12b285
Touseef Liaqat <tliaqat@vmware.com>
2017-11-20 02:48:41 -0800

    Do not re-synchronize on node expiration
    Synchronization is currently triggers couple of times
    when a node disappears from the node-group. First it
    is triggered on the unavailable state of the disappeared
    node, and then it is triggered again when we expire that
    node and remove it from our node-group completely.
    This patch removes this duplication of synchronization.
    
    Change-Id: I49eaeb759d775a6d88ecd744737f978c87482713

--- xenon-common/src/main/java/com/vmware/xenon/services/common/NodeGroupService.java
+++ xenon-common/src/main/java/com/vmware/xenon/services/common/NodeGroupService.java
@@ -54,8 +54,8 @@ public class NodeGroupService extends StatefulService {
             ServiceHostState.DEFAULT_OPERATION_TIMEOUT_MICROS / 3
     );
 
-    private enum NodeGroupChange {
-        PEER_ADDED, PEER_STATUS_CHANGE, SELF_CHANGE
+    enum NodeGroupChange {
+        PEER_ADDED, PEER_STATUS_CHANGE, SELF_CHANGE, PEER_EXPIRED
     }
 
     public static class JoinPeerRequest {
@@ -147,6 +147,10 @@ public class NodeGroupService extends StatefulService {
          * two way state merges
          */
         public Map<String, NodeState> nodes = new ConcurrentHashMap<>();
+        /**
+         * List of change types that caused the node-group state change.
+         */
+        public EnumSet<NodeGroupChange> lastChanges = EnumSet.noneOf(NodeGroupChange.class);
         /**
          * The maximum value among all reported times from the peers. If one peer has significant
          * time drift compared to others, this value will appears in the future or past, compared to local time.
@@ -957,7 +961,7 @@ public class NodeGroupService extends StatefulService {
             }
 
             if (expirationMicros > 0 && now > expirationMicros) {
-                changes.add(NodeGroupChange.PEER_STATUS_CHANGE);
+                changes.add(NodeGroupChange.PEER_EXPIRED);
                 logInfo("Removing expired unavailable node %s(%s)", l.id, l.groupReference);
                 missingNodes.add(l.id);
             }
@@ -967,6 +971,7 @@ public class NodeGroupService extends StatefulService {
             localState.nodes.remove(id);
         }
 
+        localState.lastChanges = changes;
         boolean isModified = !changes.isEmpty();
         localState.membershipUpdateTimeMicros = Math.max(
                 remotePeerState.membershipUpdateTimeMicros,
